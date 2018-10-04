24f41e9a72d07c66eaec18c8e002ad8061f118ae
Sam <sam912358@gmail.com>
2018-10-04 17:35:47 +0800

    execise 4 CSS Style binding

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -4,13 +4,33 @@ new Vue({
   	isEffect: false,
   	redClass: 'red',
   	fontClass: 'font',
-  	classValue: 'shrink'
+  	classValue: 'shrink',
+  	classValue2: 'box1',
+  	enableFlag: false,
+  	styleValue: '',
+  	progressColor: 'red'
 
   },
   methods: {
     startEffect: function() {
        this.isEffect = !this.isEffect;
+    },
+
+    startProgress: function(){
+    	var vm = this;
+    	setTimeout(function(){
+    		vm.progressColor = 'green';
+    	},	2000);
     }
 
+  },
+  computed: {
+  	progressbar: function() {
+  	  return {
+  	  	'background-color': this.progressColor,
+  	  	width: '300px',
+  	  	height: '10px',
+  	  }
+  	}
   }
 });
